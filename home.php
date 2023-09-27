8cdecc9efe0fbbc8368ffa5fc808de78c7525e9c
A-Damien <125483784+A-Damien@users.noreply.github.com>
2023-09-27 11:32:32 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> User Home Page </title>
+    <link rel="stylesheet" type="text/css" href="homeStyle.css">
+</head>
+<body>
+    <h2 style="font-family: monospace;"> Your Contact List </h2>
+    
+    <form id="form" role="search">
+        <input type="search" id="query" name="q" placeholder="Search Contact..." style="font-family: monospace;">
+        <button>
+        <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+        </button>
+    </form>
+
+    <div style="overflow-y: auto;">
+        <table>
+            <thead>
+                <tr>
+                    <th style="font-family: monospace;">First Name</th>
+                    <th style="font-family: monospace;">Last Name</th>
+                    <th style="font-family: monospace;">Email Address</th>
+                    <th style="font-family: monospace;">Phone Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+                <tr>
+                    <td style="font-family: monospace;">John</td>
+                    <td style="font-family: monospace;">Galt</td>
+                    <td style="font-family: monospace;">jg1105@gmail.com</td>
+                    <td style="font-family: monospace;">313-445-5151</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Digital Rolodex </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action="login.php" method="post">
+            <h2 style="font-family: monospace;">Welcome to Your Rolodex</h2>
+            
+            <input type="text" name="email" placeholder="Email Address" style="font-family: monospace;"><br>
+            
+            <input type="password" name="password" placeholder="Password" style="font-family: monospace;"><br>
+
+            <button type="submit" style="font-family: monospace;"> Login </button>
+            <button type="submit" style="font-family: monospace;"> Create Account </button>
+        </form>
+    </div>
+</body>
+</html>
