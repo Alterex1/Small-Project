18aad4a1f9dcafcf7cd3c34949c8b69e070cb9e0
A-Damien <125483784+A-Damien@users.noreply.github.com>
2023-09-30 14:08:37 -0400

    Add files via upload

---
+++ create.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Digital Rolodex </title>
+    <link rel="stylesheet" type="text/css" href="createStyle.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action="create.php" method="post">
+            <h2 style="font-family: monospace;">Create Account</h2>
+            
+            <input type="text" name="email" placeholder="Email Address" style="font-family: monospace;"><br>
+            
+            <input type="password" name="password" placeholder="Password" style="font-family: monospace;"><br>
+
+            <input type="password" name="password" placeholder="Repeat Password" style="font-family: monospace;"><br>
+
+            <button type="submit" style="font-family: monospace;"> Create Account </button>
+        </form>
+    </div>
+</body>
+</html>
