cef7bf5572fed294445280d3e89d62fda62c404f
A-Damien <125483784+A-Damien@users.noreply.github.com>
2023-10-02 21:19:47 -0400

    Add files via upload

--- create.php
+++ create.php
@@ -2,22 +2,22 @@
 <html>
 <head>
     <title> Digital Rolodex </title>
-    <script type="text/javascript" src="code.js"></script>
+    <script type="text/javascript" src="backend.js"></script>
     <link rel="stylesheet" type="text/css" href="createStyle.css">
 </head>
 <body>
-    <div class="form-container">
-        <form action="create.php" method="post">
+    <div id="registerDiv">
             <h2 style="font-family: monospace;">Create Account</h2>
             
             <input type="text" name="email" placeholder="Email Address" style="font-family: monospace;"><br>
             
             <input type="password" name="password" placeholder="Password" style="font-family: monospace;"><br>
 
-            <input type="password" name="rpassword" placeholder="Repeat Password" style="font-family: monospace;"><br>
-            <button type="submit" style="font-family: monospace;" onclick="addUser();"> Create Account </button>
+            <input type="password" name="password" placeholder="Repeat Password" style="font-family: monospace;"><br>
+
+            <button type="submit" style="font-family: monospace;" onclick="addUser()"> Create Account </button>
+
             <span id="addUserResult"></span>
-        </form>
     </div>
 </body>
 </html>
