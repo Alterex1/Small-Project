7fa9419f2191d64af514d57f2df1d8a361fe4fac
rcrespo11 <131306996+rcrespo11@users.noreply.github.com>
2023-10-05 03:09:25 -0400

    Update create.php

--- create.php
+++ create.php
@@ -10,11 +10,11 @@
             <h2 style="font-family: monospace;">Create Account</h2>
 
             
-            <input type="text" name="email" placeholder="Email Address" style="font-family: monospace;"><br>
+            <input type="text" id="email" placeholder="Email Address" style="font-family: monospace;"><br>
             
-            <input type="password" name="password" placeholder="Password" style="font-family: monospace;"><br>
+            <input type="password" id="password" placeholder="Password" style="font-family: monospace;"><br>
 
-            <input type="password" name="password" placeholder="Repeat Password" style="font-family: monospace;"><br>
+            <input type="password" id="rpassword" placeholder="Repeat Password" style="font-family: monospace;"><br>
 
             <button type="submit" id="createButton" style="font-family: monospace;" onclick="addUser()"> Create Account </button>
 
